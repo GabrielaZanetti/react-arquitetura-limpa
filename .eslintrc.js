680f14a6ce24d252b51a3d6b3e75eefcb888144f
Gabriela da Rosa Zanetti <36-gabriela.zanetti@users.noreply.yankee.unijui.edu.br>
2024-02-06 09:16:44 -0300

    chore: add lint-staged and husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,10 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended',
-      'prettier/@typescript-eslint',
-      'plugin:prettier/recommended',
-    ],
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    rules: {
-      // Adicione aqui as regras específicas do seu projeto
-    },
-};  
+  "extends": "standard-with-typescript",
+  "parserOptions": {
+    "project": "./tsconfig.json"
+  },
+  "rules": {
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/strinct-boolean-expressins": "off",
+  }
+}
