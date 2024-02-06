be9b119aabb69988755febeb93e9174d0c5012f0
Gabriela da Rosa Zanetti <36-gabriela.zanetti@users.noreply.yankee.unijui.edu.br>
2024-02-06 09:00:28 -0300

    chore: add configuracoes typescript e dependencias

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'jsdom',
     modulePaths: ['<rootDir>/src/'],
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
     testMatch: ['**/__tests__/**/*.test.tsx'],
---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,33 @@
+export
+interface HttpPostClient {
+    post (url: string): Promise<void>
+}
+
+class RemoteAuthentication {
+    constructor (
+        private readonly url: string,
+        private readonly HttpPostClient: HttpPostClient
+    ) {}
+
+    async auth (): Promise<void> {
+        await this.HttpPostClient.post(this.url)
+    }
+}
+
+describe('RemoteAuthentication', () => {
+    test('Should call HttpPostClient with correct URL', async () => {
+        class HttpPostClientSpy implements HttpPostClient {
+            url?: string
+
+            async post (url: string): Promise<void>{
+                this.url = url
+                return Promise.resolve();
+            }
+        }
+        const url = 'any_url';
+        const httpClient = new HttpPostClientSpy();
+        const sut = new RemoteAuthentication(url, httpClient);
+        await sut.auth();
+        expect(httpClient.url).toBe(url);
+    })
+})
