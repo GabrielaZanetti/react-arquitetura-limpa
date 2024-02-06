79f73c5ea5ba48b0532f72458361414a9cadc725
Gabriela da Rosa Zanetti <36-gabriela.zanetti@users.noreply.yankee.unijui.edu.br>
2024-02-06 09:23:31 -0300

    chore: add jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-    preset: 'ts-jest',
-    modulePaths: ['<rootDir>/src/'],
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-    testMatch: ['**/__tests__/**/*.test.tsx'],
-    transform: {
-      '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
 };
