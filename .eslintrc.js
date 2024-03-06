786be250babf2f0073b671662923c1f9139f2f88
Gabriela da Rosa Zanetti <36-gabriela.zanetti@users.noreply.yankee.unijui.edu.br>
2024-03-06 09:01:05 -0300

    chore: add react configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,21 @@
 module.exports = {
-  "extends": "standard-with-typescript",
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  },
+  "extends": [
+    "standard-with-typescript",
+    "plugin:react/recommended"
+  ],
+  "plugins": ["react"],
   "parserOptions": {
     "project": "./tsconfig.json"
   },
   "rules": {
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/strinct-boolean-expressins": "off",
+    "react/jsx-user-react": "error",
+    "react/jsx-user-vars": "error"
   }
 }
