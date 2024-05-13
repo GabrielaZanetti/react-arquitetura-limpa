b796c55578da0a1114fbe3b708c52047ee9d1957
GabrielaZanetti <gabiisctt@gmail.com>
2024-05-13 15:49:49 -0300

    test: ensure Login present valid state if email is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -56,6 +56,17 @@ describe('Login component', () => {
         expect(passwordStatus.textContent).toBe('🔴')
     })
 
+    test('Should show valid email state if Validation succeds', () => {
+        const { sut, validationStub } = makeSut()
+        validationStub.errorMensage = null
+        
+        const emailInput = sut.getByTestId('email')
+        fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+        const emailStatus = sut.getByTestId('email-status') 
+        expect(emailStatus.title).toBe('Tudo certo')
+        expect(emailStatus.textContent).toBe('🟢')
+    })
+
     test('Should show valid password state if Validation succeds', () => {
         const { sut, validationStub } = makeSut()
         validationStub.errorMensage = null
