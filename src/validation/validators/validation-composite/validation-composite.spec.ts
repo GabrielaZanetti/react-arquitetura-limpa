4fc45d47984aa8483633668120f08fa3dbbbff3a
GabrielaZanetti <gabiisctt@gmail.com>
2024-06-03 09:06:21 -0300

    test: ensure ValidationComposite returns the firtst error fund

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,14 +4,15 @@ import { ValidationComposite } from "./validation-composite"
 describe('ValidationComposite', () => {
     test('Should return error any validations fails', () => {
         const fieldValidationSpy = new FieldValidationSpy('any_field')
+        fieldValidationSpy.error = new Error('first_error_mensage')
         const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-        fieldValidationSpy2.error = new Error('any_error_mensage')
+        fieldValidationSpy2.error = new Error('second_error_mensage')
         const sut = new ValidationComposite([
             fieldValidationSpy,
             fieldValidationSpy2
         ])
 
         const error = sut.validate('any_field', 'any_value')
-        expect(error).toBe('any_error_mensage')
+        expect(error).toBe('first_error_mensage')
     })
 })
